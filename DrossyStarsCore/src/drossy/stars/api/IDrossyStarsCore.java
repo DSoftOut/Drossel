c248404c56c53689b4d75f7c17b69051368d8c56
NCrashed <NCrashed@gmail.com>
2013-08-31 20:15:19 +0400

    Initial commit. Engine separated into two parts, api and core implementation. Added dummies for client and server applications.

---
+++ DrossyStarsCore/src/drossy/stars/api/IDrossyStarsCore.java
@@ -0,0 +1,32 @@
+// Copyright (с) 2013 Gushcha Anton <ncrashed@gmail.com>
+/*
+ * This file is part of Foguan Engine.
+ * 
+ * Foguan Engine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Foguan Engine is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Foguan Engine.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package drossy.stars.api;
+
+/**
+ * The main interface of whole Drossy Stars engine. It handles all subsystems
+ * and controls mod loading.
+ * @author ncrashed
+ */
+public interface IDrossyStarsCore 
+{
+    /**
+     * Get loaded side.
+     * @return Side.SERVER if server side and Side.CLIENT if client side.
+     */
+    Side getSide();
+}

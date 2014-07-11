79ebe15af4b035d017e1cb4536db1e216e967ef6
NCrashed <ncrashed@gmail.com>
2014-07-12 02:29:41 +0400

    Shader interface and default implementation for opengl 3.3

---
+++ source/render/shader/opengl.d
@@ -0,0 +1,151 @@
+// written in the D programming language
+/*
+*   This file is part of DrossyStars.
+*   
+*   DrossyStars is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*   
+*   DrossyStars is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*   
+*   You should have received a copy of the GNU General Public License
+*   along with DrossyStars.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/**
+*   Copyright: © 2014 Anton Gushcha
+*   License: Subject to the terms of the GPL-3.0 license, as written in the included LICENSE file.
+*   Authors: Anton Gushcha <ncrashed@gmail.com>
+*/
+module render.shader.opengl;
+
+import derelict.opengl3.gl3;
+
+import render.shader.shader;
+
+/// Generates default implementation of shader interface for OpenGL
+/**
+*   To use this mixin you should provide following symbols of shader interface:
+*   - name
+*   - type
+*   - source
+*
+*   Example:
+*   ---------
+*   import util.functional;
+*   
+*   struct AShader
+*   {
+*       enum name = "TestShader";
+*       enum type = ShaderType.Fragment;
+*       string source() { return ""; }
+*       
+*       mixin addDefaultOpenGLShader!(allMembers!(typeof(this)));
+*   }
+*
+*   static assert(isShader!AShader);
+*   ---------
+*/
+mixin template addDefaultOpenGLShader(Members...)
+{
+    import std.string;
+    import std.traits;
+    import std.typetuple;
+
+    import render.shader.shader;
+    import util.log;
+    
+    private alias T = typeof(this);
+    
+    private template hasSymbol(string name)
+    {
+        enum hasSymbol = staticIndexOf!(name, Members) != -1 ||
+            __traits(compiles, { mixin("alias Identity!(T."~name~") Sym;"); });
+    }
+    
+    static assert(hasSymbol!"name", "addDefaultOpenGLShader expects "~T.stringof~" has a name enum!");
+    static assert(hasSymbol!"type", "addDefaultOpenGLShader expects "~T.stringof~" has is a type enum!");
+    static assert(hasSymbol!"source", "addDefaultOpenGLShader expects "~T.stringof~" has is a source function!");
+    
+    static if(!hasSymbol!"id")
+    {
+        private GLuint _id;
+        
+        /// Returns shader id (OpenGL)
+        GLuint id()
+        {
+            static bool flag = true;
+            if(flag)
+            {
+                _id = glCreateShader(mapShaderType(type));
+                flag = false;
+            }
+            return _id;
+        }
+        
+        /// Mapping to OpenGL shader type
+        private GLuint mapShaderType(ShaderType sht)
+        {
+            final switch(sht)
+            {
+                case(ShaderType.Fragment): return GL_FRAGMENT_SHADER;
+                case(ShaderType.Geometry): return GL_GEOMETRY_SHADER;
+                case(ShaderType.Vertex):   return GL_VERTEX_SHADER;
+                case(ShaderType.TessellationControl): return GL_TESS_CONTROL_SHADER;
+                case(ShaderType.TessellationEvaluation): return GL_TESS_EVALUATION_SHADER;
+            }
+        }
+    }
+    
+    static if(!hasSymbol!"compile")
+    {
+        void compile()
+        {
+            const(char*) sourcePtr = source.toStringz;
+            glShaderSource(id, 1, &sourcePtr, null);
+            glCompileShader(id);
+            
+            // Here exception could be thrown
+            check();
+        }
+        
+        private void check()
+        {
+            static if(hasLogging!T) logInfo("Compiling shader: ", name);
+            
+            GLint result = GL_FALSE;
+            int logLength;
+            glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+            glGetShaderiv(id, GL_INFO_LOG_LENGTH, &logLength);
+            if(logLength > 0)
+            {
+                auto buff = new char[logLength + 1];
+                glGetShaderInfoLog(id, logLength, null, buff.ptr);
+                
+                auto shaderLog = buff.ptr.fromStringz.idup;
+                static if(hasLogging!T) logError("Failed to compile shader: ", name, ". Reason: ", shaderLog);
+                throw new ShaderCompilationException(shaderLog);
+            }
+        }
+    }
+}
+version(unittest)
+{
+    import util.functional;
+    
+    struct AShader
+    {
+        enum name = "TestShader";
+        enum type = ShaderType.Fragment;
+        string source() { return ""; }
+        
+        mixin addDefaultOpenGLShader!(allMembers!(typeof(this)));
+    }
+}
+unittest
+{
+    static assert(isShader!AShader);
+}
---
+++ source/render/shader/shader.d
@@ -0,0 +1,87 @@
+// written in the D programming language
+/*
+*   This file is part of DrossyStars.
+*   
+*   DrossyStars is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*   
+*   DrossyStars is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*   
+*   You should have received a copy of the GNU General Public License
+*   along with DrossyStars.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/**
+*   Copyright: © 2014 Anton Gushcha
+*   License: Subject to the terms of the GPL-3.0 license, as written in the included LICENSE file.
+*   Authors: Anton Gushcha <ncrashed@gmail.com>
+*/
+module render.shader.shader;
+
+import std.range;
+
+import derelict.opengl3.gl3;
+
+import util.cinterface;
+
+/// Represents shader type
+enum ShaderType
+{
+    Vertex,
+    Geometry,
+    Fragment,
+    TessellationControl,
+    TessellationEvaluation,
+}
+
+/// Should be thrown when a compilation error occurs while compiling a shader
+class ShaderCompilationException : Exception
+{
+    @safe pure nothrow this(string msg, string file = __FILE__, size_t line = __LINE__, Throwable next = null)
+    {
+        super(msg, file, line, next);
+    }
+    
+    @safe pure nothrow this(string msg, Throwable next, string file = __FILE__, size_t line = __LINE__)
+    {
+        super(msg, file, line, next);
+    }
+}
+
+/**
+*   Compile-time interface for GPU shader: vertex, fragment, geometry and etc.
+*/
+struct CIShader
+{
+    /// Return shader name
+    enum string name = "";
+    
+    /// Returns shader type
+    enum ShaderType type = ShaderType.Vertex;
+    
+    /// Returning source of this shader
+    /**
+    *   That symbol could be enum or a function that loads shader code from
+    *   some file.
+    */
+    string source();
+    
+    /// Returns shader id (OpenGL)
+    GLuint id();
+    
+    /// Compiles sources in the shader
+    /**
+    *   Should throw $(B ShaderCompilationException) on compilation errors.
+    */
+    void compile();      
+}
+
+/// Checks if $(B T) is a shader program
+template isShader(T)
+{
+    enum isShader = isExpose!(T, CIShader); 
+}

b14063e60f0040969a8f6e8dd8dc00fe13b4dcdb
NCrashed <ncrashed@gmail.com>
2014-08-09 08:26:09 +0400

    Fixing compilatation under 2.066-rc2

--- source/util/string.d
+++ source/util/string.d
@@ -24,15 +24,22 @@ module util.string;
 
 import core.stdc.string;
 
-// from upcoming release of phobos
-/**
-*   Returns a D-style array of $(B char) given a zero-terminated C-style string.
-*   The returned array will retain the same type qualifiers as the input.
-*
-*   $(B Important Note:) The returned array is a slice of the original buffer.
-*   The original data is not changed and not copied.
-*/
-inout(char)[] fromStringz(inout(char)* cString) @system pure 
+static if (__VERSION__ < 2066) 
 {
-    return cString ? cString[0 .. strlen(cString)] : null;
+    // from upcoming release of phobos
+    /**
+    *   Returns a D-style array of $(B char) given a zero-terminated C-style string.
+    *   The returned array will retain the same type qualifiers as the input.
+    *
+    *   $(B Important Note:) The returned array is a slice of the original buffer.
+    *   The original data is not changed and not copied.
+    */
+    inout(char)[] fromStringz(inout(char)* cString) @system pure 
+    {
+        return cString ? cString[0 .. strlen(cString)] : null;
+    }
+}
+else
+{
+    public import std.string;
 }
